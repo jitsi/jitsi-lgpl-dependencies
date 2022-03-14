67bf2ec91834292c6ffbd1ada57c70e7a2dbfcee
Ingo Bauersachs <ingo@jitsi.org>
2022-03-14 23:57:05 +0100

    wip

--- resources/deb-build.sh
+++ resources/deb-build.sh
@@ -14,9 +14,9 @@ sudo sed -i s/pkg-config-\$target_tuple//g /usr/bin/mk-sbuild
 
 # --skip-security because: https://bugs.launchpad.net/ubuntu/+source/ubuntu-dev-tools/+bug/1955116
 if [[ "${ARCH}" != "amd64" ]]; then
-  mk-sbuild "${DIST}" --target "${ARCH}" --skip-security || sbuild-update -udc "${DIST}"-amd64-"${ARCH}"
+  mk-sbuild "${DIST}" --target "${ARCH}" --skip-security --type=file || sbuild-update -udc "${DIST}"-amd64-"${ARCH}"
 else
-  mk-sbuild "${DIST}" --skip-security || sbuild-update -udc "${DIST}"-amd64
+  mk-sbuild "${DIST}" --skip-security --type=file || sbuild-update -udc "${DIST}"-amd64
 fi
 
 mvn -B versions:set -DnewVersion="${VERSION}" -DgenerateBackupPoms=false
