f3781700ed2dcd5b1ee0eec67a32f1e96dc65595
Ingo Bauersachs <ingo@jitsi.org>
2022-04-03 17:19:14 +0200

    wip

--- resources/deb-build.sh
+++ resources/deb-build.sh
@@ -16,10 +16,11 @@ sudo sed -i s/pkg-config-\$target_tuple//g /usr/bin/mk-sbuild
 sudo tee -a /etc/fstab < "${PROJECT_DIR}/resources/sbuild-tmpfs"
 
 # --skip-security because: https://bugs.launchpad.net/ubuntu/+source/ubuntu-dev-tools/+bug/1955116
+# -debootstrap-include=default-jdk because: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=994152
 if [[ "${ARCH}" != "amd64" ]]; then
-  mk-sbuild "${DIST}" --target "${ARCH}" --skip-security --type=file || sbuild-update -udc "${DIST}"-amd64-"${ARCH}"
+  mk-sbuild "${DIST}" --target "${ARCH}" --skip-security --type=file --debootstrap-include=default-jdk || sbuild-update -udc "${DIST}"-amd64-"${ARCH}"
 else
-  mk-sbuild "${DIST}" --skip-security --type=file || sbuild-update -udc "${DIST}"-amd64
+  mk-sbuild "${DIST}" --skip-security --type=file --debootstrap-include=default-jdk || sbuild-update -udc "${DIST}"-amd64
 fi
 
 mvn -B versions:set -DnewVersion="${VERSION}" -DgenerateBackupPoms=false
